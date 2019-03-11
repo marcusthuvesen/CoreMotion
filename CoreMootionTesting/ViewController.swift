77cbe298aae94932c57a27100eaa9cf051bb4933
MarcusThu <marcus.thuvesen@gmail.com>
2019-03-11 15:33:08 +0100

    Initial Commit

---
+++ CoreMootionTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMootionTesting
+//
+//  Created by Marcus Thuvesen on 2019-03-11.
+//  Copyright © 2019 Marcus Thuvesen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
